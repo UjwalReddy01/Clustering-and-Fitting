f89a8a35705abeb38821039dd82985c55a169ab5
UjwalReddy01 <149112372+UjwalReddy01@users.noreply.github.com>
2024-01-18 18:46:18 +0000

    Update Clustering_Fitting.py

--- Clustering_Fitting.py
+++ Clustering_Fitting.py
@@ -4,22 +4,10 @@ Created on Wed Jan 18 18:14:30 2023
 
 @author: sande
 """
-
-#importing pandas module to read the file(data set) and to calculate the statistical property(Describe)
+#Importing pandas package to read the file,for deriving pandas features and stats properties
 import pandas as pd
-
-#importing numpy module to calculate the statistical properties (mean and standard deviation)
+#Importing numpy package
 import numpy as np
-
-#importing pyplot from matplotlib module to plot the visualization graphs
+#Importing matplotlib package for data plotting and visualization
 import matplotlib.pyplot as plt
-
-#importing KMeans from sklearn object to identify the clusters
-from sklearn.cluster import KMeans
-
-#importing LabelEncoder to encode the categories of the data
-from sklearn.preprocessing import LabelEncoder
-
-#importing the custom error package 
-import err_ranges as error
 
